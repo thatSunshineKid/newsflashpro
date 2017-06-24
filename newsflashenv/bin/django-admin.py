d9d6f8d2f5e941474a47b87b317d154c9c4ac28d
Josh <jodw1605@colorado.edu>
2017-06-24 16:18:57 -0600

    added login functionality

---
+++ newsflashenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/joshdwernychuk/Downloads/newsflashpro-master/newsflashenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
